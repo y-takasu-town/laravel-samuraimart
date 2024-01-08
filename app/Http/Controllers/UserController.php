ef2d3ead7de84322e0ae5acd5c18426e16fcedea
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-01-08 21:35:01 +0900

    18章

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,6 +51,29 @@ class UserController extends Controller
         return to_route('mypage');
     }
 
+        public function update_password(Request $request)
+        {
+            $validatedData = $request->validate([
+                'password' => 'required|confirmed',
+            ]);
+    
+            $user = Auth::user();
+    
+            if ($request->input('password') == $request->input('password_confirmation')) {
+                $user->password = bcrypt($request->input('password'));
+                $user->update();
+            } else {
+                return to_route('mypage.edit_password');
+            }
+    
+            return to_route('mypage');
+        }
+
+
+     public function edit_password()
+     {
+         return view('users.edit_password');
+     }
    
     
 }
