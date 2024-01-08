d4b2460c48508b6f13b5271fe7849151168b325d
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-01-08 21:02:17 +0900

    15章

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,11 +18,11 @@ class ProductController extends Controller
     public function index(Request $request)
     {
         if ($request->category !== null) {
-            $products = Product::where('category_id', $request->category)->paginate(15);
+            $products = Product::where('category_id', $request->category)->sortable()->paginate(15);
             $total_count = Product::where('category_id', $request->category)->count();
             $category = Category::find($request->category);
         } else {
-            $products = Product::paginate(15);
+            $products = Product::sortable()->paginate(15);
             $total_count = "";
             $category = null;
         }        $categories = Category::all();
