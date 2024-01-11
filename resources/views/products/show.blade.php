79ed43fef79c7d9ab70bc88ea4b4c97e81af8c5c
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-01-11 09:58:57 +0900

    1章 レビューに評価機能を追加しよう

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -68,6 +68,7 @@
             <div class="row">
                 @foreach($reviews as $review)
                 <div class="offset-md-5 col-md-5">
+                    <h3 class="review-score-color">{{ str_repeat('★', $review->score) }}</h3>
                     <p class="h3">{{$review->content}}</p>
                     <label>{{$review->created_at}} {{$review->user->name}}</label>
                 </div>
@@ -79,6 +80,14 @@
                 <div class="offset-md-5 col-md-5">
                     <form method="POST" action="{{ route('reviews.store') }}">
                         @csrf
+                        <h4>評価</h4>
+                        <select name="score" class="form-control m-2 review-score-color">
+                            <option value="5" class="review-score-color">★★★★★</option>
+                            <option value="4" class="review-score-color">★★★★</option>
+                            <option value="3" class="review-score-color">★★★</option>
+                            <option value="2" class="review-score-color">★★</option>
+                            <option value="1" class="review-score-color">★</option>
+                        </select>
                         <h4>レビュー内容</h4>
                         @error('content')
                             <strong>レビュー内容を入力してください</strong>
