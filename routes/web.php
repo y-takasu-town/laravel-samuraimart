49d42374bb34b9ac91ee1576a525b93312ee0d62
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-01-11 14:27:50 +0900

    11章 注文履歴を表示できるようにしよう

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,8 @@ Route::controller(UserController::class)->group(function () {
     Route::put('users/mypage/password', 'update_password')->name('mypage.update_password'); 
     Route::get('users/mypage/favorite', 'favorite')->name('mypage.favorite');
     Route::delete('users/mypage/delete', 'destroy')->name('mypage.destroy');
+    Route::get('users/mypage/cart_history', 'cart_history_index')->name('mypage.cart_history');
+    Route::get('users/mypage/cart_history/{num}', 'cart_history_show')->name('mypage.cart_history_show');
 });
 
 
